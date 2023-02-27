b3dc7a10610b3f5292297ff5d8a33e5c71a4af86
anastasia-s02 <86anastasias@gmail.com@github.com>
2023-02-27 16:33:45 -0500

    comment fix

--- options.py
+++ options.py
@@ -119,11 +119,11 @@ def create_nav_menu():
     return nav_choice
 
 if __name__ == "__main__":
-    # Set page title and favicon
+    # Set page title and creates navigation menu
     st.set_page_config(page_title="OPTimal", page_icon=":chart_with_upwards_trend:")
     nav_choice = create_nav_menu()
 
-    # Define the content for each page
+    # Define the content for Home page
     if nav_choice == "Home":
         st.markdown('<div class="bg-image"></div>', unsafe_allow_html=True)
 
@@ -131,16 +131,19 @@ if __name__ == "__main__":
         st.header("OPTimal!")
         st.subheader("Learn how to trade options and supercharge your portfolio")
         st.image(
-            os.path.join("/Users/macbook/Desktop/HackNYU/KnowYourOptions", "cover.jpg")
+            os.path.join("", "cover.jpg")
         )
-
+    # Define the content for page with Black-Scholes visualization 
     elif nav_choice == "Black-Scholes Visualization":
+
+	# Set page header and description
         st.title("Black-Scholes Formula Visualization")
         st.write(
             "This app lets you see how the Black-Scholes formula "
-            + "can be used to price a European call option on a stock."
+            + "can be used to price European call and put options on a stock."
         )
 
+	# Set sidebar and sliders 
         st.sidebar.title("Input Parameters")
         stock_price = st.sidebar.slider(
             "Current stock price (S), $", min_value=1, max_value=1000, value=100
@@ -162,18 +165,19 @@ if __name__ == "__main__":
             "Time to expiration (T)", min_value=0.01, max_value=1.0, value=0.5, step=0.01
         )
 
+	# Plot a graph for call price
         call_price = calculate_call_price(
             stock_price, strike_price, risk_free_rate, volatility, time_until_maturity
         )
-
         st.write(
             f"**Given the input parameters, the call option price is:** `{call_price:.2f}`"
         )
-
         call_figure = plot_black_scholes_call(
             stock_price, strike_price, risk_free_rate, volatility, time_until_maturity
         )
         st.pyplot(call_figure, dpi=150)
+
+	# Description of call price plot 
         st.write(
             "The plot above shows how the price of the call option changes with "
             + "the price of the stock. You can see that as the stock price goes up, "
@@ -181,43 +185,49 @@ if __name__ == "__main__":
             + "of the call option has the right to buy the stock at a lower price than "
             + "the market price, and thus stands to profit from the increase in the stock price."
         )
+
+	# Plot a graph for put price 
         put_price = calculate_put_price(
             stock_price, strike_price, risk_free_rate, volatility, time_until_maturity
         )
-
         st.write(
             f"**Given the input parameters, the put option price is:** `{put_price:.2f}`"
         )
-
         put_figure = plot_black_scholes_put(
             stock_price, strike_price, risk_free_rate, volatility, time_until_maturity
         )
         st.pyplot(put_figure, dpi=150)
-
+	
+	# Description of put price plot
+	st.write(
+            "The plot above shows how the price of the put option changes with "
+            + "the price of the stock. You can see that as the stock price goes up, "
+            + "the value of the put option also goes down. This is because the holder "
+            + "of the put option has the right to sell the stock at a higher price than "
+            + "the market price, and thus stands to profit from the decrease in the stock price."
+        )
+    # Define the content for page with short description of what options are 
     elif nav_choice == "What are options?":
         # Add text
         st.header("What are options?")
         st.image(
-            os.path.join("/Users/macbook/Desktop/HackNYU/KnowYourOptions", "graph.png")
+            os.path.join("", "graph.png")
         )
         st.write(
             "Options are contracts that give the holder the right, but not the obligation, "
             + "to buy or sell an underlying asset at a predetermined price, within a specified "
             + "time period. The underlying asset can be a stock, an index, a commodity, or a currency."
         )
-
         st.write(
             "There are two types of options: calls and puts. A call option gives the holder the right "
             + "to buy an underlying asset at a predetermined price, while a put option gives the holder "
             + "the right to sell an underlying asset at a predetermined price."
         )
-
         st.write(
             "The price of an option is determined by several factors, including the current price of "
             + "the underlying asset, the strike price, the time to expiration, and the volatility of "
             + "the underlying asset."
         )
-
         st.write(
             "Suppose you want to buy a call option on a stock that is currently trading at \$100. "
             + "The strike price of the option is \$110, and the option expires in one month. "
